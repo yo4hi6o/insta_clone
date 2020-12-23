daa14ed213d060ed6c1272fa745af5bed1f4be56
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2020-12-23 14:35:39 +0900

    [エラー発生] cocoapods 50.サインインが完了したら〜までコーディング終了

--- lib/view/login/screens/login_screen.dart
+++ lib/view/login/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:insta_clone/generated/l10n.dart';
 import 'package:insta_clone/style.dart';
 import 'package:insta_clone/view/login/common/compponents/button_with_icon.dart';
+import 'package:insta_clone/view/login/screens/home_screen.dart';
 import 'package:insta_clone/view_models/login_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -11,29 +13,47 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              S.of(context).appTitle,
-              style: loginTitleTextStyle,
-            ),
-            SizedBox(height: 8.0,),
-            ButtonWithIcon(
-              iconData: FontAwesomeIcons.signInAlt,
-              label: S.of(context).signIn,
-              onPressed: () => login(context),
-
-            ),
-          ],
+        child: Consumer<LoginViewModel>(
+          builder: (context, model, child) {
+            return model.isLoading
+                ? CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        S.of(context).appTitle,
+                        style: loginTitleTextStyle,
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      ButtonWithIcon(
+                        iconData: FontAwesomeIcons.signInAlt,
+                        label: S.of(context).signIn,
+                        onPressed: () => login(context),
+                      ),
+                    ],
+                  );
+          },
         ),
       ),
     );
   }
 
-  login(BuildContext context) async{
+  login(BuildContext context) async {
     final loginViewModel = Provider.of<LoginViewModel>(context, listen: false);
     await loginViewModel.signIn();
-    //todo
+    if (!loginViewModel.isSuccessful) {
+      Fluttertoast.showToast(msg: S.of(context).signInFailed);
+      return;
+    }
+    _openHomeScreen(context);
+  }
+
+  void _openHomeScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => HomeScreen()),
+    );
   }
 }
