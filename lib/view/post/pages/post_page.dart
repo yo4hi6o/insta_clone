c25545093d5dab68c5a193c7ba8d44b00b410568
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2020-12-24 13:37:27 +0900

    55.投稿画面　[PostPage]の実装をしよう

--- lib/view/post/pages/post_page.dart
+++ lib/view/post/pages/post_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:insta_clone/generated/l10n.dart';
+import 'package:insta_clone/view/login/common/compponents/button_with_icon.dart';
 
 class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("PostPage"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 90.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ButtonWithIcon(
+                iconData: FontAwesomeIcons.images,
+                label: S.of(context).gallery,
+                //todo
+                onPressed: null,
+              ),
+              SizedBox(height: 24.0,),
+              ButtonWithIcon(
+                iconData: FontAwesomeIcons.camera,
+                label: S.of(context).camera,
+                //todo
+                onPressed: null,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
