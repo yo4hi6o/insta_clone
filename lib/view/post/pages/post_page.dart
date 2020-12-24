d04413e0d61f9c9f7304d10daed8ea9a6d168e16
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2020-12-24 13:57:52 +0900

    56.投稿画面(PostPage)から投稿アップロード画面

--- lib/view/post/pages/post_page.dart
+++ lib/view/post/pages/post_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:insta_clone/generated/l10n.dart';
+import 'package:insta_clone/utils/constants.dart';
 import 'package:insta_clone/view/login/common/compponents/button_with_icon.dart';
+import 'package:insta_clone/view/post/pages/screens/post_upload_screen.dart';
 
 class PostPage extends StatelessWidget {
   @override
@@ -18,18 +20,35 @@ class PostPage extends StatelessWidget {
                 iconData: FontAwesomeIcons.images,
                 label: S.of(context).gallery,
                 //todo
-                onPressed: null,
+                onPressed: () => _openPostUploadScreen(
+                  UploadType.GALLERY,
+                  context,
+                ),
               ),
-              SizedBox(height: 24.0,),
+              SizedBox(
+                height: 24.0,
+              ),
               ButtonWithIcon(
                 iconData: FontAwesomeIcons.camera,
                 label: S.of(context).camera,
                 //todo
-                onPressed: null,
+                onPressed: () => _openPostUploadScreen(
+                  UploadType.CAMERA,
+                  context,
+                ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  _openPostUploadScreen(UploadType uploadType, BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => PostUploadScreen(uploadType: uploadType),
       ),
     );
   }
---
+++ lib/view/post/pages/screens/post_upload_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone/utils/constants.dart';
+
+class PostUploadScreen extends StatelessWidget {
+  final UploadType uploadType;
+  PostUploadScreen({this.uploadType});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
