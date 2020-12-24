684e95a244bd7999d2def323b04a7f5b81e0ce98
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2020-12-24 13:17:57 +0900

    53.[BottomNavigationBar(画面下のナビゲーションメニュー）]の設定をしよう

--- lib/view/login/screens/home_screen.dart
+++ lib/view/login/screens/home_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:insta_clone/generated/l10n.dart';
+import 'package:insta_clone/view/activities/pages/activities_page.dart';
+import 'package:insta_clone/view/feed/pages/feed_page.dart';
+import 'package:insta_clone/view/post/pages/post_page.dart';
+import 'package:insta_clone/view/profile/pages/profile_page.dart';
+import 'package:insta_clone/view/search/pages/search_page.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,11 +13,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> _pages;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      FeedPage(),
+      SearchPage(),
+      PostPage(),
+      ActivitiesPage(),
+      ProfilePage(),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(S.of(context).appTitle),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index){
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.home),
+            label: "トップニュース",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.search),
+            label: "検索",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.plusSquare),
+            label: "追加",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.heart),
+            label: "アクティビティ",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.user),
+            label: "ユーザー",
+          ),
+        ],
       ),
     );
   }
