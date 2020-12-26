6e3787bfc9885f8b5e84ed380f9e6c7de2dace11
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2020-12-26 20:27:13 +0900

    68.画像と現在位置情報の取得を受けて投稿アップロード画面を更新しよう

--- lib/view/post/pages/screens/post_upload_screen.dart
+++ lib/view/post/pages/screens/post_upload_screen.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone/generated/l10n.dart';
 import 'package:insta_clone/utils/constants.dart';
 import 'package:insta_clone/view_models/post_view_model.dart';
 import 'package:provider/provider.dart';
 
 class PostUploadScreen extends StatelessWidget {
   final UploadType uploadType;
+
   PostUploadScreen({this.uploadType});
 
   @override
   Widget build(BuildContext context) {
     final postViewModel = Provider.of<PostViewModel>(context, listen: false);
 
-    if (!postViewModel.isImagePicked && !postViewModel.isProcessing){
+    if (!postViewModel.isImagePicked && !postViewModel.isProcessing) {
       Future(() => postViewModel.pickImage(uploadType));
     }
 
-    return Scaffold(
-      body: Container(),
+    return Consumer<PostViewModel>(
+      builder: (context, model, child) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: model.isProcessing
+                ? Container()
+                : IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: _cancelPost(context),
+                  ),
+            title: model.isProcessing
+                ? Text(S.of(context).underProcessing)
+                : Text(S.of(context).post),
+            actions: <Widget>[
+              (model.isProcessing || !model.isImagePicked)
+                  ? IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: _cancelPost(context),
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.done),
+                      //todo ダイアログを出して投稿処理
+                      onPressed: null,
+                    )
+            ],
+          ),
+        );
+      },
     );
   }
+
+//todo
+  _cancelPost(BuildContext context) {}
 }
