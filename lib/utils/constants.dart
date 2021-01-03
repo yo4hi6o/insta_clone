d785651cbea21757ed656db0c0822797d1df8a0b
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2021-01-03 19:46:13 +0900

    107.[PopupMenuButton]を使って投稿画像のカスタム処理部分の大枠を実装しよう

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -12,3 +12,9 @@ enum FeedMode {
   FROM_FEED, //自分+フォローしているユーザー
   FROM_PROFILE, //プロフィール画面に表示されているユーザーのみ
 }
+
+enum PostMenu {
+  EDIT,
+  DELETE,
+  SHARE,
+}
--- lib/view/feed/components/sub/feed_post_header_part.dart
+++ lib/view/feed/components/sub/feed_post_header_part.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/data_models/post.dart';
 import 'package:insta_clone/data_models/user.dart';
+import 'package:insta_clone/generated/l10n.dart';
+import 'package:insta_clone/utils/constants.dart';
 import 'package:insta_clone/view/common/components/user_card.dart';
 
 class FeedPostHeaderPart extends StatelessWidget {
@@ -18,11 +20,42 @@ class FeedPostHeaderPart extends StatelessWidget {
   Widget build(BuildContext context) {
     return UserCard(
       photoUrl: postUser.photoUrl,
-      title: postUser.photoUrl,
+      title: postUser.inAppUserName,
       subTitle: post.locationString,
       onTap: null,
       //todo
-      trailing: null, //todo
+      trailing: PopupMenuButton(
+        icon: Icon(Icons.more_vert),
+        onSelected: (value) => onPopupMenuSelected(context, value),
+        itemBuilder: (context) {
+          if (postUser.userId == currentUser.userId) {
+            return [
+              PopupMenuItem(
+                value: PostMenu.EDIT,
+                child: Text(S.of(context).edit),
+              ),
+              PopupMenuItem(
+                value: PostMenu.DELETE,
+                child: Text(S.of(context).delete),
+              ),
+              PopupMenuItem(
+                value: PostMenu.SHARE,
+                child: Text(S.of(context).share),
+              ),
+            ];
+          } else {
+            return [
+              PopupMenuItem(
+                value: PostMenu.SHARE,
+                child: Text(S.of(context).share),
+              ),
+            ];
+          }
+        },
+      ),
     );
   }
+
+  //todo
+  onPopupMenuSelected(BuildContext context, value) {}
 }
