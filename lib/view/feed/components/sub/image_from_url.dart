7818bc64547b95541f1f3ce58345ed583e17e749
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2021-01-03 19:56:15 +0900

    108.個別投稿表示パーツの投稿画像表示部分を実装しよう（ImageFromUrl）

--- lib/view/feed/components/sub/image_from_url.dart
+++ lib/view/feed/components/sub/image_from_url.dart
@@ -1,11 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ImageFromUrl extends StatelessWidget {
   final String imageUrl;
 
   ImageFromUrl({@required this.imageUrl});
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(child: Image.network(imageUrl),));
+    if (imageUrl == null) {
+      return const Icon(Icons.broken_image);
+    } else {
+      return CachedNetworkImage(
+        imageUrl: imageUrl,
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+        fit: BoxFit.cover,
+      );
+    }
+    ;
   }
 }
