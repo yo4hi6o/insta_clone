53689e3a7aac517260e7d02a33f7b07b7fbeeded
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2021-01-08 13:50:36 +0900

    146.プロフィール画面に表示するユーザーを設定しよう

--- lib/view/profile/pages/profile_page.dart
+++ lib/view/profile/pages/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/data_models/user.dart';
 import 'package:insta_clone/utils/constants.dart';
+import 'package:insta_clone/view_models/profile_view_model.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
   final ProfileMode profileMode;
@@ -10,6 +12,12 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final profileViewModel = Provider.of<ProfileViewModel>(context, listen: false);
+    profileViewModel.setProfileUser(profileMode, selectedUser);
+
+
+    }
+
     return Scaffold(
       body: Center(
         child: Text("ProfilePage"),
--- lib/view_models/profile_view_model.dart
+++ lib/view_models/profile_view_model.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone/data_models/user.dart';
 import 'package:insta_clone/models/repositories/post_repository.dart';
 import 'package:insta_clone/models/repositories/user_repository.dart';
+import 'package:insta_clone/utils/constants.dart';
 
 class ProfileViewModel extends ChangeNotifier {
   final UserRepository userRepository;
   final PostRepository postRepository;
   ProfileViewModel({this.userRepository, this.postRepository});
+
+  User profileUser;
+  User get currentUser => UserRepository.currentUser;
+
+  void setProfileUser(ProfileMode profileMode, User selectedUser) {
+    if (profileMode == ProfileMode.MYSELF) {
+      profileUser = currentUser;
+    } else {
+      profileUser = selectedUser;
+    }
+  }
+
 }
