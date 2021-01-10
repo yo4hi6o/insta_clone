aa42e63263b3a2f2f009ec2a7eeed537bff6e435
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2021-01-10 11:17:34 +0900

    154.プロフィール詳細パーツ内のプロフィール画像パーツを実装しよう(ProfileImage)

--- lib/view/profile/components/sub/profile_image.dart
+++ lib/view/profile/components/sub/profile_image.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone/view/common/components/circle_photo.dart';
+import 'package:insta_clone/view_models/profile_view_model.dart';
+import 'package:provider/provider.dart';
 
 class ProfileImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("ProfileImage"),
-      color: Colors.blue,
+    final profileViewModel =
+        Provider.of<ProfileViewModel>(context, listen: false);
+    final profileUser = profileViewModel.profileUser;
+
+    return CirclePhoto(
+      photoUrl: profileUser.photoUrl,
+      isImageFromFile: false,
+      radius: 30.0,
     );
   }
 }
