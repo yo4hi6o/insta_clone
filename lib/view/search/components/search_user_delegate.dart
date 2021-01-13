8e67983c4593bcadee9f1a48db0a77c494b210cd
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2021-01-13 21:08:18 +0900

    175.ユーザー検索処理の結果を受けてユーザー検索画面の更新処理を実装しよう

--- lib/view/search/components/search_user_delegate.dart
+++ lib/view/search/components/search_user_delegate.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/data_models/user.dart';
+import 'package:insta_clone/view/common/components/user_card.dart';
 import 'package:insta_clone/view_models/search_view_model.dart';
 import 'package:provider/provider.dart';
 
 class SearchUserDelegate extends SearchDelegate<User> {
-
   @override
   ThemeData appBarTheme(BuildContext context) {
     final theme = Theme.of(context);
 
-    return theme.copyWith(
-      brightness: Brightness.dark
-
-    );
+    return theme.copyWith(brightness: Brightness.dark);
   }
 
   @override
@@ -50,9 +47,23 @@ class SearchUserDelegate extends SearchDelegate<User> {
 
   //todo ユーザー検索処理
   Widget _buildResults(BuildContext context) {
-    final searchViewModel = Provider.of<SearchViewModel>(context,listen: false);
+    final searchViewModel =
+        Provider.of<SearchViewModel>(context, listen: false);
     searchViewModel.searchUsers(query);
 
-    return Container();
+    return ListView.builder(
+      itemCount: searchViewModel.soughtUsers.length,
+      itemBuilder: (context, int index) {
+        final user = searchViewModel.soughtUsers[index];
+        return UserCard(
+          photoUrl: user.photoUrl,
+          title: user.inAppUserName,
+          subTitle: user.bio,
+          onTap: () {
+            close(context, user);
+          },
+        );
+      },
+    );
   }
 }
--- lib/view/search/pages/search_page.dart
+++ lib/view/search/pages/search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/generated/l10n.dart';
 import 'package:insta_clone/style.dart';
+import 'package:insta_clone/utils/constants.dart';
+import 'package:insta_clone/view/profile/screens/profile_screen.dart';
 import 'package:insta_clone/view/search/components/search_user_delegate.dart';
 
 class SearchPage extends StatelessWidget {
@@ -30,6 +32,18 @@ class SearchPage extends StatelessWidget {
       context: context,
       delegate: SearchUserDelegate(),
     );
+    //ユーザー検索結果を受けた処理
+
+    if (selectedUser != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ProfileScreen(
+            profileMode: ProfileMode.OTHER,
+            selectedUser: selectedUser,
+          ),
+        ),
+      );
+    }
   }
-  //ユーザー検索結果を受けた処理
 }
