ef1e45ce79e6fc15f79e90498d74f6f61915ded6
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2021-01-17 19:40:11 +0900

    182.WhoCaresMeScreenを開く処理を実装しよう（②：誰にフォローされたか・誰をフォローしているか）

--- lib/view/profile/components/sub/profile_records.dart
+++ lib/view/profile/components/sub/profile_records.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/generated/l10n.dart';
 import 'package:insta_clone/style.dart';
+import 'package:insta_clone/utils/constants.dart';
+import 'package:insta_clone/view/who_cares_me/screens/who_cares_me_screen.dart';
 import 'package:insta_clone/view_models/profile_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -8,7 +10,7 @@ class ProfileRecords extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final profileViewModel =
-    Provider.of<ProfileViewModel>(context, listen: false);
+        Provider.of<ProfileViewModel>(context, listen: false);
 
     return Row(
       children: <Widget>[
@@ -18,9 +20,7 @@ class ProfileRecords extends StatelessWidget {
             return _userRecordWidget(
               context: context,
               score: snapshot.hasData ? snapshot.data : 0,
-              title: S
-                  .of(context)
-                  .post,
+              title: S.of(context).post,
             );
           },
         ),
@@ -30,9 +30,8 @@ class ProfileRecords extends StatelessWidget {
             return _userRecordWidget(
               context: context,
               score: snapshot.hasData ? snapshot.data : 0,
-              title: S
-                  .of(context)
-                  .followers,
+              title: S.of(context).followers,
+              whoCaresMeMode: WhoCaresMeMode.FOLLOWED,
             );
           },
         ),
@@ -40,32 +39,54 @@ class ProfileRecords extends StatelessWidget {
             future: profileViewModel.getNumberOfFollowings(),
             builder: (context, AsyncSnapshot<int> snapshot) {
               return _userRecordWidget(
-                  context: context,
-                  score: snapshot.hasData ? snapshot.data : 0,
-                  title: S
-                  .of(context)
-                  .followings,
+                context: context,
+                score: snapshot.hasData ? snapshot.data : 0,
+                title: S.of(context).followings,
+                whoCaresMeMode: WhoCaresMeMode.FOLLOWINGS,
               );
-            }
-        ),
+            }),
       ],
     );
   }
 
-  _userRecordWidget({BuildContext context, int score, String title}) {
+  _userRecordWidget(
+      {BuildContext context,
+      int score,
+      String title,
+      WhoCaresMeMode whoCaresMeMode}) {
     return Expanded(
       flex: 1,
-      child: Column(
-        children: <Widget>[
-          Text(
-            score.toString(),
-            style: profileRecordScoreTextStyle,
-          ),
-          Text(
-            title.toString(),
-            style: profileRecordTitleTextStyle,
-          ),
-        ],
+      child: GestureDetector(
+        onTap: whoCaresMeMode == null
+            ? null
+            : () => _checkFollowUsers(context, whoCaresMeMode),
+        child: Column(
+          children: <Widget>[
+            Text(
+              score.toString(),
+              style: profileRecordScoreTextStyle,
+            ),
+            Text(
+              title.toString(),
+              style: profileRecordTitleTextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _checkFollowUsers(BuildContext context, WhoCaresMeMode whoCaresMeMode) {
+    final profileViewModel =
+        Provider.of<ProfileViewModel>(context, listen: false);
+    final profileUser = profileViewModel.profileUser;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => WhoCaresMeScreen(
+          mode: whoCaresMeMode,
+          id: profileUser.userId,
+        ),
       ),
     );
   }
